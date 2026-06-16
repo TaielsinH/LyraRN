11a6393633c86d5ef183abea741114957e558b43
TaielsinH <taielstuder@gmail.com>
2026-06-16 11:50:48 -0300

    primer cambio de pruebas

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Probando actualizacion</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
